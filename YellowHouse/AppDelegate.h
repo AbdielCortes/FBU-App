bb1c51efe91324e4533e01f81d64d2da00776d50
Abdiel Cortes <zurken@fb.com>
2020-07-13 11:26:16 -0700

    Initial Commit

---
+++ YellowHouse/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YellowHouse
+//
+//  Created by zurken on 7/13/20.
+//  Copyright © 2020 Abdiel Cortes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
