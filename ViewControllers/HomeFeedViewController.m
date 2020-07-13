ef020105fc4966ce2c2001914ee5fe051dab1b90
Abdiel Cortes <zurken@fb.com>
2020-07-13 15:17:00 -0700

    user can log out and sign in persists

---
+++ ViewControllers/HomeFeedViewController.m
@@ -0,0 +1,44 @@
+//
+//  HomeFeedViewController.m
+//  YellowHouse
+//
+//  Created by zurken on 7/13/20.
+//  Copyright © 2020 Abdiel Cortes. All rights reserved.
+//
+
+#import "HomeFeedViewController.h"
+#import "SceneDelegate.h"
+#import "LoginViewController.h"
+#import <Parse/Parse.h>
+
+@interface HomeFeedViewController ()
+
+@end
+
+@implementation HomeFeedViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)tappedLogOut:(id)sender {
+    SceneDelegate *sceneDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    sceneDelegate.window.rootViewController = loginViewController;
+    
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) { }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
