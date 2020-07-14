2e5ad514b5a505d4dafc1c429e98a1d017420880
Abdiel Cortes <zurken@fb.com>
2020-07-14 11:46:12 -0700

    created post view for regular posts

---
+++ ViewControllers/CreatePostViewController.h
@@ -0,0 +1,17 @@
+//
+//  CreatePostViewController.h
+//  YellowHouse
+//
+//  Created by zurken on 7/14/20.
+//  Copyright © 2020 Abdiel Cortes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CreatePostViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ViewControllers/CreatePostViewController.m
@@ -0,0 +1,107 @@
+//
+//  CreatePostViewController.m
+//  YellowHouse
+//
+//  Created by zurken on 7/14/20.
+//  Copyright © 2020 Abdiel Cortes. All rights reserved.
+//
+
+#import "CreatePostViewController.h"
+#import "Utilities.h"
+#import <Parse/Parse.h>
+
+@interface CreatePostViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *postImage;
+
+@property (weak, nonatomic) IBOutlet UITextView *cationTextView;
+
+@end
+
+@implementation CreatePostViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)tappedChooseImage:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (IBAction)tappedTakePhoto:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+        
+        [self presentViewController:imagePickerVC animated:YES completion:nil];
+    }
+    else { // if camera not available, show error pop up
+        [Utilities showOkAlert:self withTitle:@"Camera Not Available" withMessage:@"The camera is not available at the moment, chose an image instead."];
+    }
+}
+
+- (IBAction)tappedPost:(id)sender {
+//    [self.activityIndicator startAnimating];
+//    [Post postUserImage:self.image.image withCaption:self.captionTextView.text withCompletion:^(BOOL succeded, NSError *error) {
+//        [self.activityIndicator stopAnimating];
+//
+//        if (succeded) {
+//            self.captionTextView.text = @"";
+//            self.image.image = [UIImage imageNamed:@"image_placeholder"];
+//        }
+//        else {
+//            NSLog(@"Error when posting: %@", error);
+//        }
+//    }];
+}
+
+- (IBAction)tappedClose:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    // Get the image captured by the UIImagePickerController
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    // shrink image resolution to make feel load faster
+    UIImage *resizedImage = [self resizeImage:editedImage withSize:CGSizeMake(450, 450)];
+    
+    self.postImage.image = resizedImage;
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
