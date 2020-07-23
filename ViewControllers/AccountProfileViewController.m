7eb37a6ead5aed79a71c4f6a82407394559a528f
Abdiel Cortes <zurken@fb.com>
2020-07-23 15:06:31 -0700

    user can unfollow accounts

--- ViewControllers/AccountProfileViewController.m
+++ ViewControllers/AccountProfileViewController.m
@@ -16,6 +16,10 @@
 
 @property (weak, nonatomic) IBOutlet PFImageView *profileImage;
 
+@property (weak, nonatomic) IBOutlet UIButton *followButton;
+@property (strong, nonatomic) NSMutableArray *following;
+@property (nonatomic) BOOL isFollowing;
+
 @end
 
 @implementation AccountProfileViewController
@@ -24,45 +28,63 @@
     [super viewDidLoad];
     
     [self fetchUserData];
+    
+    self.followButton.hidden = NO;
+    if ([self.account.objectId isEqual:[PFUser currentUser].objectId]) { // user is not allowed to follow themself
+        self.followButton.hidden = YES; // hide follow button so that the user can't click it
+    }
+    else {
+        self.following = [PFUser currentUser][@"following"];
+        self.isFollowing = NO;
+        if (self.following) { // if following array is not null
+            for (PFUser *accountToFollow in self.following) { // we search through the followed accounts to see if this account is already being followed by the user
+                if ([accountToFollow.objectId isEqual:self.account.objectId]) { // user is following this account
+                    self.isFollowing = YES;
+                    [self.followButton setTitle:@"Following" forState:UIControlStateNormal];
+                }
+            }
+        }
+    }
 }
 
 - (IBAction)tappedFollow:(id)sender {
     PFUser *currentUser = [PFUser currentUser];
-    if ([self.account.objectId isEqual:currentUser.objectId]) { // user is not allowed to follow themself
-        return;
+
+    if (!self.isFollowing) { // if user is not following this account
+        if (self.following) { // if following arra is not null (array is null when this is the first account the user follows)
+            [self.following addObject:self.account]; // add acount to array
+            currentUser[@"following"] = self.following; // send array to user
+        }
+        else { // this is the first account the user is following so the following array is null
+            NSMutableArray *newArray = [[NSMutableArray alloc] init]; // create array
+            [newArray addObject:self.account]; // add account
+            currentUser[@"following"] = newArray; // send array to user
+        }
+        
+        self.isFollowing = YES;
+        [self.followButton setTitle:@"Following" forState:UIControlStateNormal]; // change button text
     }
-    
-    NSMutableArray *following = currentUser[@"following"];
-    BOOL changed = NO;
-    
-    if (following) { // user's has already followed some accounts so the array has already been instantiaded
-        BOOL foundAccount = NO;
-        for (PFUser *accountToFollow in following) { // we search through the followed accounts to see if this account is already being followed by the user
-            if ([accountToFollow.objectId isEqual:self.account.objectId]) {
-                foundAccount = YES;
+    else {
+        NSMutableArray *withoutAccount = [[NSMutableArray alloc] init]; // create new array to store all accounts except this one
+        for (PFUser *currentAccount in self.following) { // we search through the followed accounts to find this account
+            // if account is not equal to the account that we want to remove
+            if (![currentAccount.objectId isEqual:self.account.objectId]) {
+                [withoutAccount addObject:currentAccount]; // add account ot new array
             }
         }
         
-        if (!foundAccount) { // if the user is not already following this account
-            [following addObject:self.account]; // add acount to array
-            currentUser[@"following"] = following; // send array to user
-            changed = YES;
-        }
-    }
-    else { // this is the first account the user is following so the following array is null
-        NSMutableArray *newArray = [[NSMutableArray alloc] init]; // create array
-        [newArray addObject:self.account]; // add account
-        currentUser[@"following"] = newArray; // send array to user
-        changed = YES;
+        self.following = withoutAccount; // update following array
+        currentUser[@"following"] = withoutAccount; // send new array to user
+        self.isFollowing = NO;
+        [self.followButton setTitle:@"Follow" forState:UIControlStateNormal]; // change button text
     }
     
-    if (changed) { // if the account was added to the following array
-        [[PFUser currentUser] saveInBackgroundWithBlock:^(BOOL succeded, NSError *error) {
-            if (error) {
-                NSLog(@"Error occured while changing user info: %@", error);
-            }
-        }];
-    }
+    // save the changes to parse
+    [[PFUser currentUser] saveInBackgroundWithBlock:^(BOOL succeded, NSError *error) {
+        if (error) {
+            NSLog(@"Error occured while changing user info: %@", error);
+        }
+    }];
 }
 
 - (void)fetchUserData {
